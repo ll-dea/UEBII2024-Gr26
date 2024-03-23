47a1f23f4fe88f0b8e7b5c81a96e7e9cf80514fd
Dea Llapatinca <117036096+ll-dea@users.noreply.github.com>
2024-03-23 19:05:24 +0100

    header

--- Projekti/Javascript/index.js
+++ Projekti/Javascript/index.js
@@ -1,15 +1,25 @@
 let i = 0;
-        let imgArray = ['../HTML/foto/Photo13.jpg', '../HTML/foto/Photo14.jpg', '../HTML/foto/Photo15.jpg'];
+let imgArray = ['../HTML/foto/Photo13.jpg', '../HTML/foto/Photo14.jpg', '../HTML/foto/Photo15.jpg'];
 
-        function changeImg(){
-            document.getElementById('slideshow').src = imgArray[i];
+function changeImg() {
+    document.getElementById('slideshow').src = imgArray[i];
 
-            if(i< imgArray.length -1){
-                i++;
-            }
-            else{
-                i=0;
-            }
-            //setTimeout("changeImg()", 2600);
-        }
-        document.addEventListener(onload, changeImg());
+    if (i < imgArray.length - 1) {
+        i++;
+    }
+    else {
+        i = 0;
+    }
+    //setTimeout("changeImg()", 2600);
+}
+document.addEventListener(onload, changeImg());
+
+document.addEventListener("DOMContentLoaded", function () {
+    var header = document.querySelector('header');
+
+    function toggleHeaderBackground() {
+        header.classList.toggle('scrolled', window.scrollY > 50);
+    }
+
+    window.addEventListener('scroll', toggleHeaderBackground);
+});
