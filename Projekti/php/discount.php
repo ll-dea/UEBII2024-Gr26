7475e4fbec2e8303ce7f05cd5b83c3f1924f737f
ElisaGjurkaj <elisa.gjrk@gmail.com>
2024-04-19 13:39:33 +0200

    Krijimi i ofertave

---
+++ Projekti/php/discount.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv='cache-control' content='no-cache'>
+  <meta http-equiv='expires' content='0'>
+  <meta http-equiv='pragma' content='no-cache'>
+  <link rel="icon" href="img/tl.png" type="image/x-icon">
+  <title>Garden Shop</title>
+  <link rel="stylesheet" href="../CSS/About.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap">
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Truculenta:opsz,wght@12..72,100..900&display=swap" rel="stylesheet">
+
+
+
+</head>
+
+<body>
+  <!-- HEADER -->
+
+  <header class="sticky-header">
+    <h1>Gardening Shop</h1>
+    <nav>
+      <a href="index.html">Home</a>
+      <a href="Login.php">Login</a>
+      <a href="About.html">About</a>
+      <a href="Discount.php">Discount Offer</a>
+   
