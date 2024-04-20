67676e3a8a139a05f5ec55f1356c3f01513206d1
Dea Llapatinca <117036096+ll-dea@users.noreply.github.com>
2024-04-20 21:38:34 +0200

    Update

--- Projekti/php/mycart.php
+++ Projekti/php/mycart.php
@@ -124,8 +124,6 @@ if (isset($_GET['sort']) && is_array($_SESSION['cart'])) {
             <a href="home.php">Home</a>
             <a href="mycart.php">My Cart</a>
             <a href="about.php">About</a>
-            <a href="discount.php">Discount Offer</a>
-
 
         </nav>
         <div class="row" style="margin-right: 100px;">
