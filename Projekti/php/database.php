648bd4293bd8730ea4c0e10eabbf17f21f089a63
Dea Llapatinca <dea.limoni@student.uni-pr.edu>
2024-05-22 15:02:51 +0200

    Lidhja me Databze pt2

--- Projekti/php/database.php
+++ Projekti/php/database.php
@@ -8,14 +8,4 @@ $conn = mysqli_connect($hostName, $dbUser, $dbpassword, $dbName);
 if(!$conn){
     die("Couldn't connect to database!");
 }
-
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // Set PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    exit(); // Ndalo ekzekutimin në rast gabimi
-}
 ?>
