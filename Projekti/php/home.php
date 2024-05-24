8e8bac1deea79f9902306d0cfe6b9d31e144c3d5
Dea Limoni <dea.limoni@student.uni-pr.edu>
2024-05-24 14:55:19 +0200

    update home.php

--- Projekti/php/home.php
+++ Projekti/php/home.php
@@ -472,6 +472,7 @@ try{
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
+               
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -479,12 +480,11 @@ try{
             <div class="modal-body">
                 Review submitted successfully.
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
+           
         </div>
     </div>
 </div>
+
 
    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
