537dfe56e1b3218377b860fa5e06f3238612752b
ElisaGjurkaj <elisa.gjrk@gmail.com>
2024-05-27 20:03:47 +0200

    Update login.php

--- Projekti/php/login.php
+++ Projekti/php/login.php
@@ -14,14 +14,16 @@
         if(isset($_POST["login"])){
             $email=$_POST["email"];
             $password=$_POST["password"];
-            require_once"database.php";
+            require_once "database.php";
             $sql="SELECT * FROM users WHERE email='$email'";
             $result=mysqli_query($conn, $sqli);
             $user=mysqli_fetch_array($result, MYSQLI_ASSOC);
             if($user){
                 if(password_verify($password, $user["password"])){
-                    header("Location:index.php");
+                    header("Location: index.php");
                     die();
+                }else{
+                    echo "<div class='alert alert-danger'>Password does not match</div>";
                 }
 
             }else{
